506068bc69523d37c2eb2676085bbf044c172882
Ethan Liang <ethanl0518@gmail.com>
2018-11-15 18:47:29 +0800

    init

--- my_anime.js
+++ my_anime.js
@@ -12,7 +12,7 @@ absoluteOffset
 		offset: 0
 	})
 	.add({
-    	targets: '#lineDrawing .lines path',
+    	targets: '#lineDrawing .posts path',
 		strokeDashoffset: [anime.setDashoffset, 0],
 		easing: 'easeInOutSine',
 		duration: 1000,
@@ -20,5 +20,14 @@ absoluteOffset
 		direction: 'alternate',
 		loop: false,
 		offset: 1000
+  	})
+    .add({
+    	targets: '#lineDrawing .posts .extend',
+        d: 'M250 450 L600 450 L600 650 L1400 650 L1400 450',
+		easing: 'easeInOutSine',
+		duration: 1000,
+		delay: function(el, i) { return i * 250 },
+		direction: 'alternate',
+		loop: false,
+		offset: 2000
   	});
-
